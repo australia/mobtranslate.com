09c24f4e4373af73c1274cc2a27068500cf13fb9
Thomas Davis <thomasalwyndavis@gmail.com>
2024-03-27 23:09:18 +1100

    test

--- apps/web/app/page.js
+++ apps/web/app/page.js
@@ -1,3 +1,31 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const SubTitle = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const Byline = styled.div`
+  font-size: 16px;
+  font-weight: normal;
+`;
+
 export default function Page() {
-  return <main>mob translate</main>;
+  return (
+    <div>
+      <Title>Mob Translate</Title>
+      <Byline>
+        An opensource ecosystem to make Google Translate for indiginous
+        languages
+      </Byline>
+      <SubTitle>Dictionaries</SubTitle>
+    </div>
+  );
 }
