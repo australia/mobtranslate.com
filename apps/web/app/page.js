d3ad1e3e34215e2c13cbea81b0adeefcc93c556a
Thomas Davis <thomasalwyndavis@gmail.com>
2024-03-27 23:24:43 +1100

    wip

--- apps/web/app/page.js
+++ apps/web/app/page.js
@@ -1,6 +1,8 @@
 'use client';
 
+import Link from 'next/link';
 import styled from 'styled-components';
+import Head from 'next/head';
 
 const Title = styled.div`
   font-size: 24px;
@@ -17,15 +19,31 @@ const Byline = styled.div`
   font-weight: normal;
 `;
 
+const Dictionary = styled.div`
+  font-size: 16px;
+  font-weight: normal;
+`;
+
 export default function Page() {
   return (
     <div>
+      <Head>
+        <title>Mob Translate - Aboriginal Google Translate</title>
+        <meta
+          name="description"
+          content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+          key="desc"
+        />
+      </Head>
       <Title>Mob Translate</Title>
       <Byline>
         An opensource ecosystem to make Google Translate for indiginous
         languages
       </Byline>
       <SubTitle>Dictionaries</SubTitle>
+      <Dictionary>
+        <Link href="/dictionaries/kuku_yalanji">Kuku Yalanji</Link>
+      </Dictionary>
     </div>
   );
 }
